7c33790dd2757d34c85675f36f1cdef856c821ff
MarginToppson <insetml@gmail.com>
2020-11-20 10:25:32 +0200

    fix for hamburger

--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -15,32 +15,6 @@ function hamburger(){
             isOpen = false;
         }
     });
-
-    // hamburger.addEventListener('click', () => {
-    //     // if(menu.classList.contains('header__menu-hide')){
-    //     //     menu.classList.remove('header__menu-hide');
-    //     // }
-    //     // menu.classList.remove('header__menu');
-    //     overlay.style.left = "0";
-    //     menu.style.left = "0";
-    //     menu.classList.add('header__menu-mobile');
-    // });
-
-    // closeButton.addEventListener('click', () => {
-    //     menu.style.left = "-200%";
-    //     menu.classList.remove('header__menu-mobile');
-    //     overlay.style.left = "-200%";
-    // });
-
-    // navLinks.forEach(link => {
-    //     link.addEventListener('click', () => {
-    //         if(menu.classList.contains('header__menu-mobile')){
-    //             overlay.style.left = "-200%";
-    //             menu.style.left = "-200%"
-    //             menu.classList.remove('header__menu-mobile');
-    //         }
-    //     });
-    // });
 }
 
 export default hamburger;
