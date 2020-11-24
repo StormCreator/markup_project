2ce01730957cfcf37dc3ed0d40dbdc3fc61e3bdb
MarginToppson <insetml@gmail.com>
2020-11-24 20:06:39 +0200

    Slider implemented

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -3,19 +3,37 @@ function slider(){
         slidesToShow: 2,
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1201,
                 settings: {
-                centerMode: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
+                    arrows: false,
+                    slidesToShow: 1,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    centerPadding: '100px',
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    centerMode: true,
                 }
             }
-            
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
         ]
     });
 }
--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
 
     hamburger();
     slider();
+    
 });
 
 
