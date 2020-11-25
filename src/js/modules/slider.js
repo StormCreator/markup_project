15c94384b9f2ad61ce332ce1e86b9a4e82af20a6
MarginToppson <insetml@gmail.com>
2020-11-25 12:46:27 +0200

    Fixed coach slider and added dots for mobile

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,6 +1,21 @@
 function slider(){
+
+    $(".slider").on("init", function(event, slick){
+        // $(".counter").text(parseInt(slick.currentSlide + 1) + ' of ' + slick.slideCount);
+        $(".slick-count").text(`${parseInt(slick.currentSlide + 1)} of ${slick.slideCount}`);
+
+    });
+    
+    $(".slider").on("afterChange", function(event, slick, currentSlide){
+        $(".slick-count").text(`${parseInt(slick.currentSlide + 1)} of ${slick.slideCount}`);
+    });
+
+
     $('.slider').slick({
         slidesToShow: 2,
+        arrows: false,
+        // prevArrow: '<button type="button" class="slick-prev"><img src="../images/slider/prev-arrow.png"></button>',
+        // nextArrow: '<button type="button" class="slick-next"><img src="../images/slider/next-arrow.png"></button>',
         responsive: [
             {
                 breakpoint: 1201,
@@ -24,6 +39,8 @@ function slider(){
                     slidesToShow: 1,
                     arrows: false,
                     centerMode: true,
+                    dots: true,
+                    dotsClass: "mydots",
                 }
             },
             {
@@ -32,9 +49,18 @@ function slider(){
                     slidesToShow: 1,
                     arrows: false,
                     centerMode: true,
+                    dots: true,
+                    dotsClass: "mydots",
                 }
             }
         ]
+    });
+
+    $('.slick-next').on('click', function() {
+        $('.slider').slick('slickNext');
+    });
+    $('.slick-prev').on('click', function() {
+        $('.slider').slick('slickPrev');
     });
 }
 
